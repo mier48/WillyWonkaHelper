8a41d3c54ec0e38f0203e181d40787524b0b573e
Alberto <brtandhhh1595@gmail.com>
2022-05-16 13:08:20 +0200

    remove unused class

--- app/src/main/java/com/albertomier/willywonkahelper/data/model/OompaLoompaProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.albertomier.willywonkahelper.data.model
-
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class OompaLoompaProvider @Inject constructor() {
-    var oompaLoompas: List<OompaLoompaModel> = emptyList()
-}
